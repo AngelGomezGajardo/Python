a91fb9dea16a984d1424a4413334837cef5a2f2f
Ángel Gómez Gajardo <angel.g.gajardo@gmail.com>
2025-10-27 16:49:04 -0300

    Create Mysql+consultasABdd.py

---
+++ Mysql+consultasABdd.py
@@ -0,0 +1,76 @@
+# Importar librerías necesarias. NO SE DEBE IMPORTAR NINGUNA OTRA LIBRERÍA.
+import mysql.connector as db
+
+# Conexión a la base de datos
+conn = db.connect(
+  host="localhost",
+  user="root",
+  password="123456", # Cambiar por la contraseña de su base de datos
+  database="mp2"
+)
+
+# Crear cursor
+cursor = conn.cursor()
+
+# NO TOCAR. Función que encapsula lógica para ejecutar una consulta y mostrar los resultados.
+def run_query(query, header):
+  if not query or query == '':
+    print("No se definió consulta para ejecutar")
+    return
+  try:
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    print(header + '\n')
+    for row in rows:
+      print(row)
+  except Exception as e:
+    print('Consulta resultó en error: ' + str(e))
+
+
+# ---------- CONSULTA 1 ----------
+# RELLENAR SOLAMENTE LA VARIABLE consulta_1
+consulta_1 = (""" 
+SELECT COUNT(pedidos.id) AS cantidad_pedidos
+FROM pedidos
+JOIN clientes ON pedidos.id_cliente = clientes.id
+WHERE clientes.email = 'jessicaflores@example.com';
+""")
+
+print('\n\n' + '-'*10 + ' INICIO CONSULTA 1 ' + '-'*10 + '\n')
+run_query(consulta_1, 'Número de pedidos realizados por cliente con email jessicaflores@example.com:')
+print('\n' + '-'*10 + ' FIN CONSULTA 1 ' + '-'*10 + '\n')
+
+
+# ---------- CONSULTA 2 ----------
+# RELLENAR SOLAMENTE LA VARIABLE consulta_2
+consulta_2 = """
+SELECT productos.id,productos.nombre,productos.precio,productos_pedidos.cantidad 
+FROM productos_pedidos
+JOIN productos ON productos_pedidos.id_producto = productos.id 
+WHERE id_pedido = 2 
+ORDER BY productos.id ASC;
+"""
+
+print('\n\n' + '-'*10 + ' INICIO CONSULTA 2 ' + '-'*10 + '\n')
+run_query(consulta_2, 'id, nombre, precio y cantidad de productos solicitados en pedido con id 2:')
+print('\n' + '-'*10 + ' FIN CONSULTA 2 ' + '-'*10 + '\n')
+
+
+# ---------- CONSULTA 3 ----------
+# RELLENAR SOLAMENTE LA VARIABLE consulta_3
+consulta_3 = """
+SELECT pedidos.id, pedidos.direccion, pedidos.detalle, pedidos.fecha
+FROM pedidos
+JOIN productos_pedidos ON pedidos.id = productos_pedidos.id_pedido
+JOIN productos ON productos_pedidos.id_producto = productos.id
+WHERE productos.nombre = 'Tablet' AND (pedidos.fecha >= '2024-01-05' AND pedidos.fecha <= '2024-01-07')
+ORDER BY pedidos.fecha DESC;
+"""
+
+print('\n\n' + '-'*10 + ' INICIO CONSULTA 3 ' + '-'*10 + '\n')
+run_query(consulta_3, 'id, direccion, detalle y fecha de todos los pedidos entre el 5 de enero y el 7 de enero (incluyendo ambas fechas) que contengan un Tablet, ordenados por fecha descendentemente:')
+print('\n' + '-'*10 + ' FIN CONSULTA 3 ' + '-'*10 + '\n')
+
+
+cursor.close()
+conn.close()
